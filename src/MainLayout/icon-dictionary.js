2f5060aa783925cd8a7cfdd8638c2302498044fa
swaroop hashbinary <70526703+swapHash@users.noreply.github.com>
2020-12-21 13:36:26 +0530

    Add files via upload

---
+++ src/MainLayout/icon-dictionary.js
@@ -0,0 +1,75 @@
+// @flow
+
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faArrowsAlt,
+  faMousePointer,
+  faExpandArrowsAlt,
+  faGripLines,
+  faTag,
+  faPaintBrush,
+  faCrosshairs,
+  faDrawPolygon,
+  faVectorSquare,
+  faHandPaper,
+  faSearch,
+  faMask,
+  faEdit,
+} from "@fortawesome/free-solid-svg-icons"
+import FullscreenIcon from "@material-ui/icons/Fullscreen"
+import AccessibilityNewIcon from "@material-ui/icons/AccessibilityNew"
+
+const faStyle = { marginTop: 4, width: 16, height: 16, marginBottom: 4 }
+
+export const iconDictionary = {
+  select: () => (
+    <FontAwesomeIcon
+      style={faStyle}
+      size="xs"
+      fixedWidth
+      icon={faMousePointer}
+    />
+  ),
+  pan: () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faHandPaper} />
+  ),
+  zoom: () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faSearch} />
+  ),
+  "show-tags": () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faTag} />
+  ),
+  "create-point": () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faCrosshairs} />
+  ),
+  "create-box": () => (
+    <FontAwesomeIcon
+      style={faStyle}
+      size="xs"
+      fixedWidth
+      icon={faVectorSquare}
+    />
+  ),
+  "create-polygon": () => (
+    <FontAwesomeIcon
+      style={faStyle}
+      size="xs"
+      fixedWidth
+      icon={faDrawPolygon}
+    />
+  ),
+  "create-expanding-line": () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faGripLines} />
+  ),
+  "show-mask": () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faMask} />
+  ),
+  "modify-allowed-area": () => (
+    <FontAwesomeIcon style={faStyle} size="xs" fixedWidth icon={faEdit} />
+  ),
+  "create-keypoints": AccessibilityNewIcon,
+  window: FullscreenIcon,
+}
+
+export default iconDictionary
